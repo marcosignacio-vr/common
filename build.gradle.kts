62f8e5c5347603a61415d0a9fa455e4052bdb3a1
marc antonyc <marcosignacio.vr@gmail.com>
2025-10-06 10:43:02 -0300

    update build

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,3 +63,11 @@ kotlin {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+	enabled = false
+}
+
+tasks.getByName<Jar>("jar") {
+	enabled = true
+}
